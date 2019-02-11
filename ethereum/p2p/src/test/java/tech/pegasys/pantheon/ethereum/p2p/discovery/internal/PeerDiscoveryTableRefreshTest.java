9ab67606c61fdd9b36528db4ad30e8256e550550
S. Matthew English <s.matthew.english@gmail.com>
2019-02-11 10:32:59 -0500

    tableRefreshSingleNode

--- ethereum/p2p/src/test/java/tech/pegasys/pantheon/ethereum/p2p/discovery/internal/PeerDiscoveryTableRefreshTest.java
+++ ethereum/p2p/src/test/java/tech/pegasys/pantheon/ethereum/p2p/discovery/internal/PeerDiscoveryTableRefreshTest.java
@@ -14,7 +14,7 @@ package tech.pegasys.pantheon.ethereum.p2p.discovery.internal;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -25,9 +25,9 @@ import tech.pegasys.pantheon.crypto.SECP256K1.KeyPair;
 import tech.pegasys.pantheon.ethereum.p2p.discovery.DiscoveryPeer;
 import tech.pegasys.pantheon.ethereum.p2p.discovery.PeerDiscoveryStatus;
 import tech.pegasys.pantheon.ethereum.p2p.discovery.PeerDiscoveryTestHelper;
-import tech.pegasys.pantheon.ethereum.p2p.peers.Endpoint;
 import tech.pegasys.pantheon.ethereum.p2p.peers.PeerBlacklist;
 import tech.pegasys.pantheon.util.Subscribers;
+import tech.pegasys.pantheon.util.bytes.BytesValue;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -70,45 +70,47 @@ public class PeerDiscoveryTableRefreshTest {
     // Send a PING, so as to add a Peer in the controller.
     final PingPacketData ping =
         PingPacketData.create(peers.get(1).getEndpoint(), peers.get(0).getEndpoint());
-    final Packet packet = Packet.create(PacketType.PING, ping, keypairs.get(1));
-    controller.onMessage(packet, peers.get(1));
+    final Packet pingPacket = Packet.create(PacketType.PING, ping, keypairs.get(1));
+    controller.onMessage(pingPacket, peers.get(1));
 
     // Wait until the controller has added the newly found peer.
     assertThat(controller.getPeers()).hasSize(1);
 
-    /**
-     * The mechanism to refresh the peer table is managed by the {@link PeerDiscoveryController} but
-     * is effectuated in the {@link RecursivePeerRefreshState}. As the recursive refresh process
-     * proceeds in rounds it is necessary to first cancel the current round, before a refresh (and
-     * accordingly the instantiation of a fresh round) can occur. This action will precipitate a new
-     * "bonding round", which is characterized by the dissemination of {@link PacketType.PING}
-     * messages between peers.
-     */
+    // Simulate a PONG message from peer 0.
+    final PongPacketData pongPacketData =
+        PongPacketData.create(localPeer.getEndpoint(), pingPacket.getHash());
+    final Packet pongPacket = Packet.create(PacketType.PONG, pongPacketData, keypairs.get(0));
+
     final ArgumentCaptor<Packet> captor = ArgumentCaptor.forClass(Packet.class);
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 5; i++) {
+
+      controller.onMessage(pongPacket, peers.get(0));
+
       controller.getRecursivePeerRefreshState().cancelCurrentRound();
       timer.runPeriodicHandlers();
       controller.getPeers().forEach(p -> p.setStatus(PeerDiscoveryStatus.KNOWN));
+      controller.onMessage(pingPacket, peers.get(1));
     }
-    verify(outboundMessageHandler, atLeast(5)).send(any(), captor.capture());
+    verify(outboundMessageHandler, atLeast(5)).send(eq(peers.get(1)), captor.capture());
     final List<Packet> capturedFindNeighborsPackets =
         captor.getAllValues().stream()
-            .filter(p -> p.getType().equals(PacketType.PING))
+            .filter(p -> p.getType().equals(PacketType.FIND_NEIGHBORS))
             .collect(Collectors.toList());
     assertThat(capturedFindNeighborsPackets.size()).isEqualTo(5);
 
     // Collect targets from find neighbors packets
-    final List<Endpoint> targets = new ArrayList<>();
+    final List<BytesValue> targets = new ArrayList<>();
     for (final Packet captured : capturedFindNeighborsPackets) {
-      final Optional<PingPacketData> maybeData = captured.getPacketData(PingPacketData.class);
+      Optional<FindNeighborsPacketData> maybeData =
+          captured.getPacketData(FindNeighborsPacketData.class);
       assertThat(maybeData).isPresent();
-      final PingPacketData pingPacketData = maybeData.get();
-      targets.add(pingPacketData.getTo());
+      final FindNeighborsPacketData neighborsData = maybeData.get();
+      targets.add(neighborsData.getTarget());
     }
 
     assertThat(targets.size()).isEqualTo(5);
 
-    // All targets are the same.
-    assertThat(new HashSet<>(targets).size()).isEqualTo(1);
+    // All targets are unique.
+    assertThat(targets.size()).isEqualTo(new HashSet<>(targets).size());
   }
 }
