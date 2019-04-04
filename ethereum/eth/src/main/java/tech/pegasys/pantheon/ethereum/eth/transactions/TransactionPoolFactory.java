79ac8c46f6de35bb5cc21acafbc797ae4500d7c5
S. Matthew English <s.matthew.english@gmail.com>
2019-04-04 18:32:04 -0400

    initial scaffolding

--- ethereum/eth/src/main/java/tech/pegasys/pantheon/ethereum/eth/transactions/TransactionPoolFactory.java
+++ ethereum/eth/src/main/java/tech/pegasys/pantheon/ethereum/eth/transactions/TransactionPoolFactory.java
@@ -14,6 +14,7 @@ package tech.pegasys.pantheon.ethereum.eth.transactions;
 
 import tech.pegasys.pantheon.ethereum.ProtocolContext;
 import tech.pegasys.pantheon.ethereum.core.PendingTransactions;
+import tech.pegasys.pantheon.ethereum.core.Synchronizer;
 import tech.pegasys.pantheon.ethereum.core.TransactionPool;
 import tech.pegasys.pantheon.ethereum.eth.manager.EthContext;
 import tech.pegasys.pantheon.ethereum.eth.messages.EthPV62;
@@ -23,6 +24,42 @@ import tech.pegasys.pantheon.metrics.MetricsSystem;
 import java.time.Clock;
 
 public class TransactionPoolFactory {
+
+  public static TransactionPool createTransactionPool(
+          final ProtocolSchedule<?> protocolSchedule,
+          final ProtocolContext<?> protocolContext,
+          final EthContext ethContext,
+          final Clock clock,
+          final int maxPendingTransactions,
+          final MetricsSystem metricsSystem,
+          final Synchronizer synchronizer) {
+    final PendingTransactions pendingTransactions =
+            new PendingTransactions(maxPendingTransactions, clock, metricsSystem);
+
+    final PeerTransactionTracker transactionTracker = new PeerTransactionTracker();
+    final TransactionsMessageSender transactionsMessageSender =
+            new TransactionsMessageSender(transactionTracker);
+
+    final TransactionPool transactionPool =
+            new TransactionPool(
+                    pendingTransactions,
+                    protocolSchedule,
+                    protocolContext,
+                    new TransactionSender(transactionTracker, transactionsMessageSender, ethContext),
+                    synchronizer);
+
+    final TransactionsMessageHandler transactionsMessageHandler =
+            new TransactionsMessageHandler(
+                    ethContext.getScheduler(),
+                    new TransactionsMessageProcessor(transactionTracker, transactionPool));
+
+    ethContext.getEthMessages().subscribe(EthPV62.TRANSACTIONS, transactionsMessageHandler);
+    protocolContext.getBlockchain().observeBlockAdded(transactionPool);
+    ethContext.getEthPeers().subscribeDisconnect(transactionTracker);
+    return transactionPool;
+  }
+
+
 
   public static TransactionPool createTransactionPool(
       final ProtocolSchedule<?> protocolSchedule,
