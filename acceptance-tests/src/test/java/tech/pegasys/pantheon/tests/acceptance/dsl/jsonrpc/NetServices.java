b3aa2a8e99507d53c1788162b2a42f22f9decfe4
S. Matthew English <s.matthew.english@gmail.com>
2019-05-13 06:34:41 -0400

    hkl III

--- acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/jsonrpc/NetServices.java
+++ acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/jsonrpc/NetServices.java
@@ -1,14 +1,17 @@
 package tech.pegasys.pantheon.tests.acceptance.dsl.jsonrpc;
 
+import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.Response;
 import org.web3j.protocol.core.methods.response.NetVersion;
 import tech.pegasys.pantheon.tests.acceptance.dsl.condition.Condition;
 import tech.pegasys.pantheon.tests.acceptance.dsl.node.Node;
 import tech.pegasys.pantheon.tests.acceptance.dsl.node.RunnableNode;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.JsonRequestFactories;
+import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.NetServicesJsonRpcRequestFactory;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.Transaction;
 
 import java.net.URI;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,35 +27,49 @@ public class NetServices {
 
                 System.out.println("888");
 
-                final Boolean result =
+                final Map<String, Map<String, String>> result =
                         node.execute(
-                                new Transaction<Boolean>() {
+                                new Transaction<Map<String, Map<String, String>>>() {
                                     @Override
-                                    public Boolean execute(JsonRequestFactories requestFactories) {
+                                    public Map<String, Map<String, String>> execute(JsonRequestFactories requestFactories) {
 
                                         System.out.println("777");
 
-                                        Response<Boolean> resp = null;
+                                        NetServicesJsonRpcRequestFactory.NetServicesResponse c = null;
                                         try {
 
-                                            System.out.println("netVersion: " + requestFactories.net().netVersion().send().getNetVersion());
+                                            //System.out.println("netVersion: " + requestFactories.net().netVersion().send().getNetVersion());
 
-                                            System.out.println("netVersion: " + requestFactories.net().netListening().send().isListening());
+                                            //System.out.println("netVersion: " + requestFactories.net().netListening().send().isListening());
 
                                             URI enodeUrl = ((RunnableNode) x).enodeUrl();
 
-                                            System.out.println("-->: " + requestFactories.netServices().netServices(enodeUrl).send().getJsonrpc());
+                                            System.out.println("xxx");
 
+                                            NetServicesJsonRpcRequestFactory s = requestFactories.netServices();
 
-                                            resp = requestFactories.admin().adminAddPeer(enodeUrl).send();
-                                            assertThat(resp).isNotNull();
-                                            assertThat(resp.hasError()).isFalse();
+                                            System.out.println("yyy");
+
+                                            Request<?, NetServicesJsonRpcRequestFactory.NetServicesResponse> t = s.netServices(enodeUrl);
+
+                                            System.out.println("zzz");
+
+                                            c = t.send();
+
+                                            //System.out.println("--> " + c.getResult());
+
+                                            System.out.println("aaa");
+
+                                            //resp = requestFactories.admin().adminAddPeer(enodeUrl).send();
+                                            //assertThat(resp).isNotNull();
+                                            //assertThat(resp.hasError()).isFalse();
                                         } catch (final Exception ignored) {
                                         }
-                                        return resp.getResult();
+                                        return c.getResult();
                                     }
                                 });
-                assertThat(result).isTrue();
+                //assertThat(result).isTrue();
+                System.out.println("--> " + result);
             }
         };
     }
