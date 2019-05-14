c8744310ba8c70abb15483f092f6ade6ec0404fe
S. Matthew English <s.matthew.english@gmail.com>
2019-05-14 12:45:54 -0400

    ~

--- acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/transaction/net/NetServicesTransaction.java
+++ acceptance-tests/src/test/java/tech/pegasys/pantheon/tests/acceptance/dsl/transaction/net/NetServicesTransaction.java
@@ -1,28 +1,40 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package tech.pegasys.pantheon.tests.acceptance.dsl.transaction.net;
 
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.methods.response.NetVersion;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.JsonRequestFactories;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.NetServicesJsonRpcRequestFactory;
 import tech.pegasys.pantheon.tests.acceptance.dsl.transaction.Transaction;
 
 import java.util.Map;
 
+import org.web3j.protocol.core.Request;
+
 public class NetServicesTransaction implements Transaction<Map<String, Map<String, String>>> {
 
-    NetServicesTransaction() {}
+  NetServicesTransaction() {}
 
-    @Override
-    public Map<String, Map<String, String>> execute(
-            final JsonRequestFactories requestFactories) {
-        NetServicesJsonRpcRequestFactory.NetServicesResponse netServicesResponse = null;
-        try {
-            NetServicesJsonRpcRequestFactory netServicesJsonRpcRequestFactory = requestFactories.netServices();
-            Request<?, NetServicesJsonRpcRequestFactory.NetServicesResponse> request =
-                    netServicesJsonRpcRequestFactory.netServices();
-            netServicesResponse = request.send();
-        } catch (final Exception ignored) {
-        }
-        return netServicesResponse.getResult();
+  @Override
+  public Map<String, Map<String, String>> execute(final JsonRequestFactories requestFactories) {
+    NetServicesJsonRpcRequestFactory.NetServicesResponse netServicesResponse = null;
+    try {
+      NetServicesJsonRpcRequestFactory netServicesJsonRpcRequestFactory =
+          requestFactories.netServices();
+      Request<?, NetServicesJsonRpcRequestFactory.NetServicesResponse> request =
+          netServicesJsonRpcRequestFactory.netServices();
+      netServicesResponse = request.send();
+    } catch (final Exception ignored) {
     }
+    return netServicesResponse.getResult();
+  }
 }
