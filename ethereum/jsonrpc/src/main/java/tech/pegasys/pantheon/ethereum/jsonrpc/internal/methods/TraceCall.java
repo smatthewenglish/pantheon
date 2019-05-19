fd3dde36c3b83e82fc1897f16554ad447c483270
S. Matthew English <s.matthew.english@gmail.com>
2019-05-19 02:37:50 -0400

    eth call test

---
+++ ethereum/jsonrpc/src/main/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/methods/TraceCall.java
@@ -0,0 +1,112 @@
+package tech.pegasys.pantheon.ethereum.jsonrpc.internal.methods;
+
+import tech.pegasys.pantheon.ethereum.jsonrpc.RpcMethod;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.JsonRpcRequest;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.exception.InvalidJsonRpcParameters;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.parameters.BlockParameter;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.parameters.JsonCallParameter;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.parameters.JsonRpcParameter;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.queries.BlockchainQueries;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.response.JsonRpcErrorResponse;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.response.JsonRpcResponse;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.response.JsonRpcSuccessResponse;
+import tech.pegasys.pantheon.ethereum.transaction.CallParameter;
+import tech.pegasys.pantheon.ethereum.transaction.TransactionSimulator;
+
+import java.util.OptionalLong;
+
+import static tech.pegasys.pantheon.ethereum.jsonrpc.JsonRpcErrorConverter.convertTransactionInvalidReason;
+
+public class TraceCall implements JsonRpcMethod {
+
+    private final TransactionSimulator transactionSimulator;
+    private final BlockchainQueries blockchainQueries;
+    private final JsonRpcParameter parameters;
+
+    public TraceCall(
+            final BlockchainQueries blockchainQueries,
+            final TransactionSimulator transactionSimulator,
+            final JsonRpcParameter parameters) {
+        this.transactionSimulator = transactionSimulator;
+        this.blockchainQueries = blockchainQueries;
+        this.parameters = parameters;
+    }
+
+    @Override
+    public String getName() {
+        return RpcMethod.TRACE_CALL.getMethodName();
+    }
+
+    protected BlockchainQueries blockchainQueries() {
+        return blockchainQueries;
+    }
+
+    protected JsonRpcParameter parameters() {
+        return parameters;
+    }
+
+    protected BlockParameter blockParameter(final JsonRpcRequest request) {
+        return parameters().required(request.getParams(), 1, BlockParameter.class);
+    }
+
+    protected Object resultByBlockNumber(final JsonRpcRequest request, final long blockNumber) {
+        final CallParameter callParams = validateAndGetCallParams(request);
+
+        return transactionSimulator
+                .process(callParams, blockNumber)
+                .map(
+                        result ->
+                                result
+                                        .getValidationResult()
+                                        .either(
+                                                (() ->
+                                                        new JsonRpcSuccessResponse(
+                                                                request.getId(), result.getOutput().toString())),
+                                                reason ->
+                                                        new JsonRpcErrorResponse(
+                                                                request.getId(), convertTransactionInvalidReason(reason))))
+                .orElse(validRequestBlockNotFound(request));
+    }
+
+    private JsonRpcSuccessResponse validRequestBlockNotFound(final JsonRpcRequest request) {
+        return new JsonRpcSuccessResponse(request.getId(), null);
+    }
+
+    public JsonRpcResponse response(final JsonRpcRequest request) {
+        return new JsonRpcSuccessResponse(request.getId(), findResultByParamType(request));
+    }
+
+    private CallParameter validateAndGetCallParams(final JsonRpcRequest request) {
+        final JsonCallParameter callParams =
+                parameters().required(request.getParams(), 0, JsonCallParameter.class);
+        if (callParams.getTo() == null) {
+            throw new InvalidJsonRpcParameters("Missing \"to\" field in call arguments");
+        }
+        return callParams;
+    }
+
+    Object pendingResult(final JsonRpcRequest request) {
+        return null;
+    }
+
+    Object latestResult(final JsonRpcRequest request) {
+        return resultByBlockNumber(request, blockchainQueries.headBlockNumber());
+    }
+
+    Object findResultByParamType(final JsonRpcRequest request) {
+        final BlockParameter blockParam = blockParameter(request);
+
+        final Object result;
+        final OptionalLong blockNumber = blockParam.getNumber();
+        if (blockNumber.isPresent()) {
+            result = resultByBlockNumber(request, blockNumber.getAsLong());
+        } else if (blockParam.isLatest()) {
+            result = latestResult(request);
+        } else {
+            // If block parameter is not numeric or latest, it is pending.
+            result = pendingResult(request);
+        }
+
+        return result;
+    }
+}
