63bff9881eaa0a2edd364c7219ade8b5c592307a
S. Matthew English <s.matthew.english@gmail.com>
2019-05-19 22:19:32 -0400

    TraceCallHyperTest

--- ethereum/jsonrpc/src/test/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/methods/TraceCallHyperTest.java
+++ ethereum/jsonrpc/src/test/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/methods/TraceCallHyperTest.java
@@ -1,8 +1,34 @@
 package tech.pegasys.pantheon.ethereum.jsonrpc.internal.methods;
 
+import org.junit.Before;
 import org.junit.Test;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.parameters.JsonRpcParameter;
+import tech.pegasys.pantheon.ethereum.jsonrpc.internal.queries.BlockchainQueries;
+import tech.pegasys.pantheon.ethereum.mainnet.ProtocolSchedule;
+import tech.pegasys.pantheon.ethereum.transaction.TransactionSimulator;
 
 public class TraceCallHyperTest {
+
+    TraceCall traceCall;
+    TransactionSimulator transactionSimulator;
+    BlockchainQueries blockchainQueries;
+    ProtocolSchedule<?> protocolSchedule;
+    JsonRpcParameter jsonRpcParameter;
+
+    @Before
+    public void setUp() {
+
+        blockchainQueries = new BlockchainQueries();
+
+        // protocolSchedule = ...
+
+        transactionSimulator = new TransactionSimulator(
+                blockchainQueries.getBlockchain(),
+                blockchainQueries.getWorldStateArchive(),
+                protocolSchedule);
+
+        traceCall = new TraceCall(blockchainQueries, transactionSimulator, jsonRpcParameter);
+    }
 
     @Test
     public void wireTogetherFireTogether() {
