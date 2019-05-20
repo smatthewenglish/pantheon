9fc0037f0c5f77176af79138adcbb3503a1f80f0
S. Matthew English <s.matthew.english@gmail.com>
2019-05-19 22:11:09 -0400

    wireTogetherFireTogether

---
+++ ethereum/jsonrpc/src/test/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/methods/TraceCallHyperTest.java
@@ -0,0 +1,11 @@
+package tech.pegasys.pantheon.ethereum.jsonrpc.internal.methods;
+
+import org.junit.Test;
+
+public class TraceCallHyperTest {
+
+    @Test
+    public void wireTogetherFireTogether() {
+        System.out.println("hello world...");
+    }
+}
